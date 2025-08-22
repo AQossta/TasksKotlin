3fc4de8e07db34b0667dbbfc88654c03d93ea20c
Aqossta <131615337+AQossta@users.noreply.github.com>
2025-08-22 22:44:33 +0500

    ThirdTask

---
+++ Tasks/ThirdTasks.main.kts
@@ -0,0 +1,25 @@
+val text = readLine()!!
+val freq = mutableMapOf<Char, Int>()
+for (ch in text) {
+    freq[ch] = freq.getOrDefault(ch, 0) + 1
+}
+println("$freq")
+
+val arr = arrayOf(2, 7, 4, 9, 12, 5, 6)
+val evenSum = arr.filter { it % 2 == 0 }.sum()
+println("$evenSum")
+
+val word = readLine()!!
+val isPalindrome = word == word.reversed()
+println("$isPalindrome")
+
+val arr2 = arrayOf(15, 3, 9, 21, 7, 12)
+val sortedArr = arr2.sorted()
+println("$sortedArr")
+
+val arr3 = arrayOf(1, 3, 5, 3, 7, 1, 9, 5)
+val counts = mutableMapOf<Int, Int>()
+for (num in arr3) {
+    counts[num] = counts.getOrDefault(num, 0) + 1
+}
+println("$counts")

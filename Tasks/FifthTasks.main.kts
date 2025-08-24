5b84ff876465290bcd5147af59fc7d6fc4666b6e
Aqossta <131615337+AQossta@users.noreply.github.com>
2025-08-24 23:01:17 +0500

    fifthtasks

--- Tasks/FifthTasks.main.kts
+++ Tasks/FifthTasks.main.kts
@@ -13,3 +13,22 @@ for (i in arr2.indices) {
     }
 }
 println("$arr2")
+
+
+val arr3 = arrayOf(1, 3, 5, 7, 9, 11, 13)
+val terget = readLine()!!.toInt()
+var left = 0
+var right = arr3.size - 1
+var found = false
+while (left <= right) {
+    val mid = (left + right) / 2
+    if (arr3[mid] == terget) {
+        found = true
+        break
+    } else if (arr3[mid] < terget) {
+        left = mid + 1
+    } else {
+        right = mid - 1
+    }
+}
+println("$found")

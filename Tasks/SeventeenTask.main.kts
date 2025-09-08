85bb1fc1cc96d09d830bbc40f2f686737ed54745
Aqossta <131615337+AQossta@users.noreply.github.com>
2025-09-08 23:53:49 +0500

    seventeentask

---
+++ Tasks/SeventeenTask.main.kts
@@ -0,0 +1,6 @@
+val a = readLine()!!.trim().toInt()
+val b = readLine()!!.trim().toInt()
+println(if (a < b) a else b)
+
+val n = readLine()!!.trim().toInt()
+println(if (n % 2 == 0) "Even" else "Odd")
